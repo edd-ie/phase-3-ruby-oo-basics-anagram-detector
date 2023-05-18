3289cc62b46b3f7c73e8321025c27fb5598d7ff8
edd-ie <ogendoeddie@gmail.com>
2023-05-18 16:01:28 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    attr_accessor :word
+    
+    def match(val)
+        val.select{|x| x.chars.sort == @word.chars.sort}
+    end
+end
+
+tst = Anagram.new("listen")
+puts tst.match(%w[enlists google inlets banana])
